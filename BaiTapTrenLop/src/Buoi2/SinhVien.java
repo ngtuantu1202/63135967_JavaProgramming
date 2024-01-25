cf0718e429bb54efd3721bed9ea8e989202931e7
ngtuantu1202 <125531012+ngtuantu1202@users.noreply.github.com>
2024-01-25 07:12:35 +0700

    Buoi2

---
+++ BaiTapTrenLop/src/Buoi2/SinhVien.java
@@ -0,0 +1,24 @@
+package Buoi2;
+
+public class SinhVien {
+	String mssv;
+	String hoTen;
+	public SinhVien(String mssv, String hoTen) {
+		super();
+		this.mssv = mssv;
+		this.hoTen = hoTen;
+	}
+	public String getMssv() {
+		return mssv;
+	}
+	public void setMssv(String mssv) {
+		this.mssv = mssv;
+	}
+	public String getHoTen() {
+		return hoTen;
+	}
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
+	}
+	
+}
